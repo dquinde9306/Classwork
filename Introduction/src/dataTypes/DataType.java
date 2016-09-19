2c5774127202069cdb6633532e83a69a81c55b53
d <d@d-PC>
2016-09-19 16:12:30 -0400

    HW

--- Introduction/src/dataTypes/DataType.java
+++ Introduction/src/dataTypes/DataType.java
@@ -10,7 +10,57 @@ public class DataType {
 		System.out.println(3+5.0/2+5*2);
 		System.out.println(3.0+5/2+5*2);
 		System.out.println((int)(3.0 + 5)/(2 + 5 * 2));
-	
+		// the placement of the double causes the answer to change because it causes different rounding: 5.0/2 = 2.5 but 5/2 = 2 and 8/12 is 0
+		if(42 == 42.0) System.out.println("42 equals 42.0");
+		
+		double d1 = 4.64; 
+		double d2 = 2.0;
+		double d3 = 2.64;
+
+		System.out.println("d1 : " + d1); 
+		System.out.println("d2 : " + d2); 
+		System.out.println("d3 : " + d3);  
+		System.out.println("d1 - d2 : " + (d1 - d2));
+		
+//		String s = 1; // 1 is an int but it is trying to return as a string
+//		System.out.println(s);
+//		
+//		String s = (String)1; // can't change an int into a string
+//		System.out.println(s);
+//		
+//		String s = "1";
+//		System.out.println(s);
+//		
+//		String s = ""+1;
+//		System.out.println(s);
+//		
+//		String s = "Happy "+18+"th birthday!";
+//		System.out.println(s);
+//		
+//		String s = "Happy 18th birthday!";
+//		System.out.println(s);
+
+//		String s = "Happy "+54/3+"th birthday!";
+//		System.out.println(s);
+//		
+		String s = "Happy "+(54/3)+"th birthday!";
+		System.out.println(s);
+//		
+//		int m = 22;
+//		int n = 7;
+//		System.out.println("m/n is "+(m/n));
+//		
+//		int m = 22;
+//		int n = 7;
+//		System.out.println("m/n is "+((double)m/n));
+//		
+		
+		
+		
+		
+		
+
+
 	
 	}
 
