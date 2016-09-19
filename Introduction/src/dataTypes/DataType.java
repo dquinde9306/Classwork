a168cd516704455ae6655fa4c478cb8a50d34580
Student 6 <Student 6@10.8.33.173>
2016-09-19 13:15:03 -0400

    data types and math

---
+++ Introduction/src/dataTypes/DataType.java
@@ -0,0 +1,16 @@
+package dataTypes;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		System.out.println(5.0/2); // double divided by integer
+		System.out.println((double)5/2);// casting
+		System.out.println(5/2);
+		// The first two are using doubles while the third is an int divided by an int. Therefore it rounds
+		System.out.println(3+5.0/2+5*2);
+		System.out.println(3.0+5/2+5*2);
+	
+	
+	}
+
+}
