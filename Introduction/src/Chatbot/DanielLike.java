11fc71131f94c50c5d4118d2134ff9e623756d1b
Student 6 <Student 6@10.8.33.173>
2016-09-27 13:14:35 -0400

    ChatBot next steps

---
+++ Introduction/src/Chatbot/DanielLike.java
@@ -0,0 +1,41 @@
+package Chatbot;
+
+public class DanielLike implements Topic {
+	private boolean inLikeLoop;
+	private String likeResponse;
+ 	
+	
+	
+	@Override
+	public void talk() {
+		DanielQMain.print("what are some things you like?");
+		inLikeLoop= true;
+		while(inLikeLoop){
+			likeResponse = DanielQMain.getInput();
+			int likePsn = DanielQMain.findKeyword(likeResponse, "like", 0);
+			
+			if(likePsn >=0){
+				String thingsLiked = likeResponse.substring(likePsn+5);
+				DanielQMain.print(" You are such an intersting person because you like " + thingsLiked);
+				if(DanielQMain.findKeyword(thingsLiked, "school", 0)>=0){
+					inLikeLoop = false;
+					DanielQMain.school.talk();
+					
+				}
+				else{
+					inLikeLoop = false;
+					DanielQMain.talkForever();
+				}
+
+		
+			}
+			else{
+				DanielQMain.print("I don't understand you");
+			}
+			
+		}
+
+		
+	}
+
+}
--- Introduction/src/Chatbot/DanielQMain.java
+++ Introduction/src/Chatbot/DanielQMain.java
@@ -8,6 +8,7 @@ public class DanielQMain {
 	static boolean inLoop;
 	static String response;
 	static Topic school;
+	static Topic like;
 	
 
 	public static void main(String[] args) {
@@ -33,12 +34,17 @@ public class DanielQMain {
 		while(inLoop){
 			print("greetings " + user + " How are you?");
 			response = getInput();
-			if(findKeyword(response, "good", 0)){
+			if(findKeyword(response, "good", 0) >= 0){
 				print("I'm so happy you're good.");
 			}
+			else if(findKeyword(response, "like", 0) >= 0){
+				inLoop = false;
+				like.talk();
+				
+			}
 			
-			else if(response.indexOf("school")>=0){
-				inLoop = false;// exit this loop
+			else if(findKeyword(response, "good", 0) >= 0){
+				inLoop = false; // exit this loop
 				school.talk();
 				
 			}
@@ -50,32 +56,67 @@ public class DanielQMain {
 	}
 	
 	
-	public static boolean findKeyword(String searchString, String key, int startIndex) {
+	public static int findKeyword(String searchString, String key, int startIndex) {
 		//delete white space
 		String phrase = searchString.trim();
 		//set all letters to lower case
 		phrase = phrase.toLowerCase();
 		key = key.toLowerCase();
+	//	System.out.println("The Phrase is " + phrase);
+	//	System.out.println("The key is " + key);
+		
 		// find position of key
 		int psn = phrase.indexOf(key);
 		//keep looking for the word until you find the right context
+//		System.out.println("The position found is " + psn);
 		
 		while(psn >= 0 ){
 			String before = " ";
 			String after = " ";
 			if(psn + key.length() < phrase.length()){
-				after = phrase.substring(psn + key.length(), psn + key.length() +1).toLowerCase();
+				after = phrase.substring(psn + key.length(), psn + key.length()+1);
+		//		System.out.println("The character after " + key + " is " + after);
 			}
 			if(psn> 0){
-				before = phrase.substring(psn,psn-1).toLowerCase();
+				before = phrase.substring(psn-1,psn).toLowerCase();
+		//		System.out.println("The character before " + key + " is " + before);
+
 			}
 			if(before.compareTo("a") < 0 && after.compareTo("a") < 0){
-				psn = phrase.indexOf(key,psn+1);
+	//			System.out.println(key + " was found at " + psn);
+				if(noNegation(phrase, psn))				
+				return psn;
+				
 			}
+			psn = phrase.indexOf(key,psn+1);
+//			System.out.println(key + " was not found. Checking " + psn);
 		}
-			
+		return -1;
+	}
+
+	//helper method a method that contributes functionality to another method
+	// helper methods are common when you need to do the same things repeatedly
+	// method is private because it is only used by the method it is helping
+	
+	private static boolean noNegation(String phrase, int index) {
+		//check for word no (3 characters)
+		if(index - 3 >= 0 && phrase.substring(index -3, index).equals("no ")){
+			return false;
+		}
 		
-		return false;
+		if(index - 4 >= 0 && phrase.substring(index -4, index).equals("not ")){
+			return false;
+		}
+		
+		if(index - 6 >= 0 && phrase.substring(index -6, index).equals("never ")){
+			return false;
+		}
+		
+		if(index - 4 >= 0 && phrase.substring(index -4, index).equals("n't ")){
+			return false;
+		}
+		
+		return true;
 	}
 
 
@@ -139,6 +180,7 @@ public class DanielQMain {
 	public static void createTopics() {
 		input = new Scanner(System.in);
 		school = new School();
+		like = new DanielLike();
 		
 	}
 	
